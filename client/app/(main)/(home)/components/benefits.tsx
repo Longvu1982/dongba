a94eb2ae769b1195e154cd6169cff879748928f8
Long Vu (Kris Nguyen) <89139559+Longvu1982@users.noreply.github.com>
2025-08-20 22:41:29 +0700

    feat(home): update benefits

--- client/app/(main)/(home)/components/benefits.tsx
+++ client/app/(main)/(home)/components/benefits.tsx
@@ -1,7 +1,30 @@
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Check, FastForward } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+
+const steps = [
+  {
+    id: 1,
+    title: "Gửi Yêu Cầu Cho Chúng Tôi",
+    description:
+      "Gửi yêu cầu của bạn cho chúng tôi và nhận được sự hỗ trợ chuyên môn, các khuyến nghị phù hợp và thông tin chi tiết.",
+  },
+  {
+    id: 2,
+    title: "Nhận Thời Gian Giao Hàng Và Báo Giá",
+    description:
+      "Nhận báo giá chi tiết cùng thời gian giao hàng dự kiến cho đơn hàng của bạn.",
+  },
+  {
+    id: 3,
+    title: "Xác Nhận Thanh Toán",
+    description:
+      "Để xác nhận đơn hàng của bạn và bắt đầu sản xuất, hai bên sẽ thảo luận về phương thức thanh toán và sau khi hoàn thành, lịch trình dự án của bạn sẽ được đảm bảo và cho phép chúng tôi bắt đầu mua sắm vật liệu và công tác chuẩn bị.",
+  },
+];
 
 const Benefits = () => {
   return (
@@ -56,6 +79,49 @@ const Benefits = () => {
           </div>
         </div>
         <Separator className="bg-neutral-600" />
+
+        <div className="grid grid-cols-2 gap-[240px] py-[50px] items-center">
+          <div className="space-y-10">
+            <h2 className="big-title leading-14">
+              Từ Yêu Cầu Tới <br /> Sản Xuất - Khởi Động <br /> Dự Án Suôn Sẻ
+            </h2>
+            <p className="text-neutral-500">
+              Biến yêu cầu và bản thiết kế của bạn thành hiện thực mỗi khi bạn
+              chi tiêu hoặc tiêu tiền ở nước ngoài. Bắt đầu chỉ trong vài phút.
+            </p>
+            <div className="relative aspect-[2.1] overflow-hidden rounded-sm">
+              <Image
+                fill
+                className="object-cover"
+                src="/benefit-2.jpg"
+                alt="benefit-2"
+              />
+            </div>
+          </div>
+
+          <div className="space-y-8">
+            {steps.map((step) => (
+              <div key={step.id} className="flex items-start gap-5">
+                <div className="bg-secondary size-9 rounded-sm text-primary text-sm shrink-0 flex items-center justify-center">
+                  {step.id}
+                </div>
+                <div>
+                  <p className="text-neutral-900 text-[20px] font-[800] mb-4">
+                    {step.title}
+                  </p>
+                  <span className="text-neutral-500 text-[18px]">
+                    {step.description}
+                  </span>
+                </div>
+              </div>
+            ))}
+            <Button variant="outline">
+              <Link href="#" className="uppercase">
+                Liên hệ chúng tôi
+              </Link>
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
